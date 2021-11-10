969055c8dcbd67453c4cec7f1c1804ecb99d9ee6
Sree-cool <78275933+Sree-cool@users.noreply.github.com>
2021-11-10 15:01:10 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function draw(){
+
+}
+
+function clearCanvas(){
+    background("white");
+}
+
+function preload(){
+    
+}
+
+function setup(){
+    canvas = createCanvas(280,280);
+    canvas.center();
+    background("white");
+}
