599166b12160d114d80fab090321bb6e547d8022
Sree-cool <78275933+Sree-cool@users.noreply.github.com>
2021-11-14 14:01:39 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,59 @@
-function draw(){
-
+#score{
+    float: left;
+    margin-left: 5px;
+    font-size: 20px;
+    font-family: 'Oswald', sans-serif;
 }
-
-function clearCanvas(){
-    background("white");
+#timer{
+    float: right;
+    margin-right: 5px;
+    font-size: 20px;
+    font-family: 'Oswald', sans-serif;
 }
-
-function preload(){
-    
-}
-
-function setup(){
-    canvas = createCanvas(280,280);
-    canvas.center();
-    background("white");
-}
+body{
+    background-color: rgb(255, 217, 0);
+  }
+  
+  h1
+  {
+    font-family: 'Oswald';
+    color:rgb(0, 0, 0);
+    background-color: cornsilk;
+  }
+  
+  #show_sketch
+  {
+    float:right;
+  }
+  
+  #timer{
+    float: right;
+  }
+  
+  #sketch_to_be_drawn{
+    color: red;
+  }
+  
+  canvas 
+    {
+      border:2px solid white;
+      border-radius:20px;
+      margin-top:135px;
+      animation-name: example;
+      animation-duration: 4s;
+      animation-iteration-count: infinite;
+  }
+  @keyframes example {
+  
+    from {box-shadow: 1px 1px 38px white;}
+    to {box-shadow: 1px 1px 38px grey;}
+  
+  }
+  
+  
+  p, span
+  {
+      padding-top:10px;
+      font-family: 'Oswald';
+      font-size:25px;
+  }
